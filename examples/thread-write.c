817633c37397e85eff0fd95bb3a14a1454fde79d
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2020-03-03 09:32:04 +0000

    multithreaded example

--- examples/thread-write.c
+++ examples/thread-write.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
 
 void *thread(void *arg)
@@ -7,6 +8,8 @@ void *thread(void *arg)
     (void) arg;
 
     printf("Secondary thread: working\n");
+    sleep(1);
+    printf("Secondary thread: done\n");
 
     return NULL;
 }
@@ -15,18 +18,14 @@ int main(int argc, char *argv[])
 {
     (void) argc; (void) argv;
 
-    /* POSIX threads are a rather fat layer above the kernel.  */
     printf("Initial thread: launching a thread\n");
 
-    /* What syscalls are used for thread creation? */
     pthread_t thr;
     if (0 != pthread_create(&thr, NULL, thread, NULL)) {
         fprintf(stderr, "Initial thread: failed to create a thread");
         exit(EXIT_FAILURE);
     }
-    printf("Initial thread: message from the initial thread\n");
 
-    /* What syscalls are used for thread joining? */
     printf("Initial thread: joining a thread\n");
     if (0 != pthread_join(thr, NULL)) {
         fprintf(stderr, "Initial thread: failed to join a thread");
